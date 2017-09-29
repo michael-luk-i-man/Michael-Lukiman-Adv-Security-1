b3a21ba5886235ffcbe9c2a63a00ce7ee23f235a
Ashkara <luck.michael@nyu.edu>
2017-09-29 18:41:35 -0400

    echo

--- sub.php
+++ sub.php
@@ -28,22 +28,10 @@ if(!mysql_select_db($database))
 						
 		if(!$result)
 		{
-			echo '<span class="red">Something went wrong while sending your response. Please contact michael.</span><br><br>
-';
-			echo '<form action="submit.php" method="post">
-			 <input type="text" placeholder="NetID (asl777)" name="netid"></input><br>
-			<textarea type="text" height="100px"  placeholder="your voice is valuable" name="replycontent"></textarea><br>
-			 <button type="submit" formmethod="post">加油</button>
-			</form>';
+			
 		}
 		else
 		{
-			echo '<span class="green">Your voice has been heard.</span><br><br>
-';
-			echo '<form action="submit.php" method="post">
-			 <input type="text" placeholder="NetID (asl777)" name="netid"></input><br>
-			<textarea type="text" height="100px"  placeholder="your voice is valuable" name="replycontent"></textarea><br>
-			 <button type="submit" formmethod="post">加油</button>
-			</form>';
+			
 		}
 ?></body></html>
